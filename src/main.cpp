84d110158097721aaa0eefcefe9e986e21691742
Peter Sims <ragtimesims@gmail.com>
2025-02-24 13:20:57 -0500

    a few trailing ch8 edits

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,7 @@
-// #include <iostream>
-// #include <fstream>
+// #include <print>
 //
-// #include "shared/vector_generators.h"
 //
 //
 // int main() {
 //
-//     // Image
-//
-//     std::ofstream outfile("test.png");
-//
-//     int image_width = 256;
-//     int image_height = 256;
-//
-//     // Render
-//
-//     outfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-//
-//     for (int j = 0; j < image_height; j++) {
-//         for (int i = 0; i < image_width; i++) {
-//             auto r = double(i) / (image_width-1);
-//             auto g = double(j) / (image_height-1);
-//             auto b = 0.0;
-//
-//             int ir = int(255.999 * r);
-//             int ig = int(255.999 * g);
-//             int ib = int(255.999 * b);
-//
-//            outfile << ir << ' ' << ig << ' ' << ib << '\n';
-//         }
-//     }
 // }
